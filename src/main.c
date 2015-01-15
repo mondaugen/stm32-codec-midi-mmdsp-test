cf70c3435ce42d00b1a198d9bfd0fc245df7c623
Nichoas Esterer <nicholas.esterer@gmail.com>
2015-01-14 20:03:16 -0500

    Code to test poly voice manager has been integrated, it builds, but hasn't been tested. Actually, we know that it doesn't work yet.

--- src/main.c
+++ src/main.c
@@ -22,6 +22,8 @@
 #include "mm_wavtab.h"
 #include "mm_sigconst.h"
 #include "wavetables.h" 
+#include "mm_poly_voice_manage.h"
+#include "mmpv_tesp.h" 
 
 #define MIDI_BOTTOM_NOTE 48
 #define MIDI_TOP_NOTE    60
@@ -30,8 +32,9 @@
 #define BUS_NUM_CHANS 1
 #define BUS_BLOCK_SIZE (CODEC_DMA_BUF_LEN / CODEC_NUM_CHANNELS)
 
-#define ATTACK_TIME 2 
+#define ATTACK_TIME  2
 #define RELEASE_TIME 2 
+#define SHORT_RELEASE_TIME 0.2
 
 //extern MMSample GrandPianoFileDataStart;
 //extern MMSample GrandPianoFileDataEnd;
@@ -40,36 +43,37 @@ static MIDIMsgBuilder_State_t lastState;
 static MIDIMsgBuilder midiMsgBuilder;
 static MIDI_Router_Standard midiRouter;
 
-MMTrapEnvedSamplePlayer spsps[MIDI_NUM_NOTES];
+static MMTrapEnvedSamplePlayer spsps[MIDI_NUM_NOTES];
+
+static MMPolyManager *pvm;
+
+static MMWavTab samples;
 
 void MIDI_note_on_do(void *data, MIDIMsg *msg)
 {
-    if ((msg->data[1] < MIDI_TOP_NOTE) && (msg->data[1] >= MIDI_BOTTOM_NOTE)) {
-        MMTrapEnvedSamplePlayer *tesp = (MMTrapEnvedSamplePlayer*)data 
-            + msg->data[1] - MIDI_BOTTOM_NOTE;
-        MMEnvedSamplePlayer_getSamplePlayerSigProc(tesp).interp =
-            MMInterpMethod_CUBIC;
-        MMEnvedSamplePlayer_getSamplePlayerSigProc(tesp).index = 0;
-        MMEnvedSamplePlayer_getSamplePlayerSigProc(tesp).rate =
-            pow(2., (msg->data[1] - 69.) / 10.) * 440.0 / WAVTABLE_FREQ;
-        MMSigProc_setState(
-                &MMEnvedSamplePlayer_getSamplePlayerSigProc(tesp),
-                MMSigProc_State_PLAYING);
-        MMTrapezoidEnv_init(&MMTrapEnvedSamplePlayer_getTrapezoidEnv(tesp),
-            0, (MMSample)msg->data[2] / 127., ATTACK_TIME, RELEASE_TIME);
-        MMEnvelope_startAttack(&MMTrapEnvedSamplePlayer_getTrapezoidEnv(tesp));
-    }
+    MMPvtespParams *params = MMPvtespParams_new();
+    params->paramType = MMPvtespParamType_NOTEON;
+    params->note = (MMSample)msg->data[1];
+    params->amplitude = (MMSample)msg->data[2] / 127.;
+    params->interpolation = MMInterpMethod_CUBIC;
+    params->index = 0;
+    params->attackTime = ATTACK_TIME;
+    /* this is the time a note that is stolen will take to decay */
+    params->releaseTime = SHORT_RELEASE_TIME; 
+    params->samples = &samples;
+    params->loop = 1;
+    MMPolyManager_noteOn(pvm, (void*)params, MMPolyManagerSteal_FALSE);
     MIDIMsg_free(msg);
 }
 
 void MIDI_note_off_do(void *data, MIDIMsg *msg)
 {
-    if ((msg->data[1] < MIDI_TOP_NOTE) && (msg->data[1] >= MIDI_BOTTOM_NOTE)) {
-        MMTrapEnvedSamplePlayer *tesp = (MMTrapEnvedSamplePlayer*)data 
-            + msg->data[1] - MIDI_BOTTOM_NOTE;
-        MMEnvelope_startRelease(
-                &MMTrapEnvedSamplePlayer_getTrapezoidEnv(tesp));
-    }
+    MMPvtespParams *params = MMPvtespParams_new();
+    params->paramType = MMPvtespParamType_NOTEOFF;
+    params->note = (MMSample)msg->data[1];
+    params->amplitude = (MMSample)msg->data[2] / 127.;
+    params->releaseTime = RELEASE_TIME;
+    MMPolyManager_noteOff(pvm, (void*)params);
     MIDIMsg_free(msg);
 }
 
@@ -106,9 +110,11 @@ int main(void)
     WaveTable_init();
 
     /* Give access to samples of sound as wave table */
-    MMWavTab samples;
     samples.data = sampleFileDataStart;
     samples.length = WAVTABLE_LENGTH_SAMPLES; // sampleFileDataEnd - sampleFileDataStart;
+
+    /* Make poly voice manager */
+    pvm = MMPolyManager_new(MIDI_NUM_NOTES);
 
     /* Enable MIDI hardware */
     MIDI_low_level_setup();
@@ -119,18 +125,11 @@ int main(void)
     /* set up the MIDI router to trigger samples */
     MIDI_Router_Standard_init(&midiRouter);
     for (i = 0; i < MIDI_NUM_NOTES; i++) {
+        /* Initialize sample player */
         MMTrapEnvedSamplePlayer_init(&spsps[i], outBus, BUS_BLOCK_SIZE, 
                 1. / (MMSample)CODEC_SAMPLE_RATE);
-        MMEnvedSamplePlayer_getSamplePlayerSigProc(&spsps[i]).samples = &samples;
-        MMEnvedSamplePlayer_getSamplePlayerSigProc(&spsps[i]).loop = 1;
-        MMSigProc_setState(
-            &MMEnvedSamplePlayer_getSamplePlayerSigProc(&spsps[i]),
-            MMSigProc_State_DONE);
-        MMSigProc_setDoneAction(
-            &MMEnvedSamplePlayer_getSamplePlayerSigProc(&spsps[i]),
-            MMSigProc_DoneAction_NONE);
-        MMTrapezoidEnv_init(&MMTrapEnvedSamplePlayer_getTrapezoidEnv(&spsps[i]),
-            0, 1, ATTACK_TIME, RELEASE_TIME);
+        /* Make new poly voice and add it to the poly voice manager */
+        MMPolyManager_addVoice(pvm, i, (MMPolyVoice*)MMPvtesp_new(&spsps[i])); 
         /* insert in signal chain after sig const*/
         MMSigProc_insertAfter(&sigConst, &spsps[i]);
     }
